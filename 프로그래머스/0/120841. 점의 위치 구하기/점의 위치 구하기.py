1913a5968da50c50f55bd3cbf2ffc6544ee390d3
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-01-18 15:38:02 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,9 @@
+def solution(dot):#dot a =[2,3]
+    if dot[0]>0 and dot[1] > 0 :
+        return 1
+    elif dot[0]>0 and dot[1]<0:
+        return 4
+    elif dot[0]<0 and dot[1]>0:
+        return 2
+    elif dot[0]<0 and dot[1]<0:
+        return 3
