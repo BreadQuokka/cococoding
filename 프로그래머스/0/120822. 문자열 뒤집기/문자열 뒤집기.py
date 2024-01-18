d1277d9b98786184142098422c8618bcdf4fd309
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-01-18 13:41:25 +0900

    Update 문자열 뒤집기.py
    새로운 풀이, 주석참고

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,8 +1,22 @@
 def solution(my_string):
     a=list(my_string)
     newlist=[]
-    for i in reversed(a):
+    for i in reversed(a): ##reverse 함
         newlist.append(i)
     b= "".join(newlist)
         
     return b
+
+
+#혹은
+def solution(my_string):
+    a=list(my_string)
+    newlist=[]
+    while a:
+        newlist.append(a.pop())
+        print(a)
+    b= "".join(newlist)
+        
+    return b
+
+solution("zoooooo")
