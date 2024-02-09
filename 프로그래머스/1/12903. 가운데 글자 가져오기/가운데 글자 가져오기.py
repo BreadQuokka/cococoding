cb9822c3a612345a925c4cc15bbd185340be1c65
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-02-09 22:15:54 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    A=len(s)//2
+    
+    if len(s)%2==0:
+        return s[A-1:A+1]
+    else:
+        return s[A]
+        
+        
+    
+    answer = ''
+    return answer
