c1e09222549c2e1dd73b9feded6cdd71138e05a6
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-02-09 21:17:59 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,3 +1,18 @@
 def solution(my_string):
     a=my_string.swapcase()
     return a
+
+
+
+# string은 덧셈이 된다
+
+
+def solution(my_string):
+    answer = ''
+
+    for i in my_string:
+        if i.isupper():
+            answer+=i.lower()
+        else:
+            answer+=i.upper()
+    return answer
