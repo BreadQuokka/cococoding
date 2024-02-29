1618729ded5d18982f86948104a7a9f3067a7cdd
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-02-29 17:27:05 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    if len(s)==4 or len(s)==6:
+        try:
+            int(s)
+        except:
+            return False
+
+        return True
+    else:
+        return False
+   
