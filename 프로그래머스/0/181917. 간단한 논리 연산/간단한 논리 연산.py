f89affb4848101512f68ab65bdba44dfc5f124b8
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2024-04-09 16:53:48 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -0,0 +1,9 @@
+def solution(x1, x2, x3, x4):
+    if (x1==True)|(x2==True):
+        if (x3==True)|(x4==True):
+            return True
+        else:
+            return False
+    else:
+        return False
+    
