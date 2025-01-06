1586c39979ea99adb8b43687f6fd11ac02dfe2cc
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2025-01-06 18:21:24 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,17 +1,6 @@
-from itertools import combinations
 def solution(nums):
-#     box=[]
-#     long= len(nums)
-#     a = list(combinations(nums,long//2))
-#     for i in a:
-#         if i not in box:
-#             box.append(i)
-#     box2=[]        
-#     for i in box:
-#         b= list(set(i))
-#         box2.append(len(b))
         
-    a=list(set(nums))
+    a=list(set(nums))# [3,2,4]
     if len(a) >= len(nums)//2:
         ans= len(nums)//2
     else:
