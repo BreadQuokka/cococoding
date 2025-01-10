5c268ae55fcaa405e92fdd254fc37aeb54575b94
JaewonHan <144408860+BreadQuokka@users.noreply.github.com>
2025-01-10 20:35:07 +0900

    [Bronze II] Title: 다이얼, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,23 +1,24 @@
-abc = "abcdefghijklmnopqrstuvwxyz".upper()
+abc= "abcdefghijklmnopqrstuvwxyz".upper()
 str = input()
-box = []
-
+box=[]
 for i in str:
-    if i in abc[:3]:       # ABC
+    if i in abc[:3]:
         box.append(3)
-    elif i in abc[3:6]:    # DEF
+    if i in abc[3:6]:
         box.append(4)
-    elif i in abc[6:9]:    # GHI
-        box.append(5)
-    elif i in abc[9:12]:   # JKL
+    if i in abc[6:9]:
+        box.append(5)# 다이얼4
+    if i in abc[9:12]:
         box.append(6)
-    elif i in abc[12:15]:  # MNO
+    if i in abc[12:15]:
         box.append(7)
-    elif i in abc[15:19]:  # PQRS
+    if i in abc[15:19]:
         box.append(8)
-    elif i in abc[19:22]:  # TUV
+    if i in abc[19:22]:
         box.append(9)
-    elif i in abc[22:]:    # WXYZ
+    if i in abc[22:]:
         box.append(10)
-
 print(sum(box))
+
+
+
